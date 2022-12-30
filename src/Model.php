72d3fa0b2aefca26da9cb2a15f3d1e37fcb6dfbc
Sven Grübel <sven.gruebel@gmx.ch>
2022-12-31 00:51:40 +0100

    Add data and attribute storage for model class

--- src/Model.php
+++ src/Model.php
@@ -2,6 +2,7 @@
 
 namespace Getpastthemonkey\DbLink;
 
+use LogicException;
 use PDO;
 
 abstract class Model
@@ -10,11 +11,17 @@ abstract class Model
 
     abstract protected static function get_attributes(): array;
 
+    private array $data;
+
     private readonly PDO $PDO;
+
+    private readonly array $attributes;
 
     public function __construct()
     {
         $this->PDO = DatabaseManager::getPDO();
+        $this->data = [];
+        $this->attributes = static::get_attributes();
     }
 
     //////////////////////////////////////////////////
@@ -28,5 +35,35 @@ abstract class Model
     public function save(): void
     {
         // TODO: Implement saving (creation and updating)
+    }
+
+    //////////////////////////////////////////////////
+    /// Magic method overloading
+
+    public function __get(string $name): mixed
+    {
+        $this->enforce_has_attribute($name);
+        return $this->data[$name];
+    }
+
+    public function __set(string $name, mixed $value): void
+    {
+        $this->enforce_has_attribute($name);
+        $this->data[$name] = $value;
+    }
+
+    //////////////////////////////////////////////////
+    /// Internal methods
+
+    private function enforce_has_attribute(string $attr): void
+    {
+        if (!$this->has_attribute($attr)) {
+            throw new LogicException("Model class \"".static::class."\" has no attribute \"".$attr."\"");
+        }
+    }
+
+    private function has_attribute(string $attr): bool
+    {
+        return array_key_exists($attr, $this->attributes);
     }
 }
