cc286b072a953b5976cbe0833c991d6f8cbb3bfb
Sven Grübel <sven.gruebel@gmx.ch>
2023-01-05 17:06:18 +0100

    Implement database connection handling

--- src/DatabaseManager.php
+++ src/DatabaseManager.php
@@ -2,6 +2,7 @@
 
 namespace Getpastthemonkey\DbLink;
 
+use LogicException;
 use PDO;
 
 final class DatabaseManager
@@ -12,7 +13,19 @@ final class DatabaseManager
     {
         if (is_null(self::$PDO)) {
             // Create new PDO connection
-            // TODO: Implement me!
+            $config_file = __DIR__."/dblink-config.php";
+            if (!is_file($config_file)) {
+                throw new LogicException("No configuration file found. Tried $config_file");
+            }
+
+            require_once $config_file;
+
+            $function = "create_pdo";
+            if (!function_exists($function)) {
+                throw new LogicException("Config file does not contain the function \"$function\"");
+            }
+
+            self::$PDO = call_user_func($function);
         }
 
         return self::$PDO;
